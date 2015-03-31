f384df6c296ea404aff9c4fad7ae0cdeccb8a257
Jeremiah Androscavage <jcandroscavage@hotmail.com>
2015-03-30 23:07:23 -0400

    Create CharacterClass.php

---
+++ CharacterClass.php
@@ -0,0 +1,18 @@
+<?php
+class CharacterClass
+{
+    public $firstName;
+    public $lastName;
+    public $gender;
+    public $height;
+    public $weight;
+
+    public $strength;
+    public $fortitude;
+    public $agility;
+    public $intellect;
+    public $spirit;
+    public $reputation;
+
+    public $health;
+}
